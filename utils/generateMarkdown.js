3391425889da4d3c33ff5b104b5876d45f2ffe85
njacques47 <nojacques94@gmail.com>
2022-03-05 18:45:54 -0500

    worked on a function to generate a table of contents based on user selection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,33 +1,61 @@
 // renders table of contents based on user input
 const renderTOC = () => {
-  switch (toc) {
-    case simple:
-      
+    let readmeType = data.readme;
+
+    if (readmeType === 'Detailed') {
       return `
-      ## Table of Contents
-      - [Installation](#installation)
-      - [Usage](#usage)
-      - [Credits](#credits)
-      - [License](#license)
-      `;
-    
-    case detailed:
+        ## Table of Contents
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [Credits](#credits)
+        - [License](#license)
+        - [Badges](#badges)
+        - [Features](#features)
+        - [Contributions](#contributions)
+        - [Tests](#tests)
+        - [Contact](#contact)
+        `;
+    } else if (readmeType === 'Simple ToC') {
       return `
-      ## Table of Contents
-      - [Installation](#installation)
-      - [Usage](#usage)
-      - [Credits](#credits)
-      - [License](#license)
-      - [Badges](#badges)
-      - [Features](#features)
-      - [Contributions](#contributions)
-      - [Tests](#tests)
-      - [Contact](#contact)
-      `;
-  
-    default:
+        ## Table of Contents
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [Credits](#credits)
+        - [License](#license)
+        `;
+    } else {
       return false;
-  }
+    };
+
+
+  // switch (readme) {
+  //   case 'Simple ToC':
+      
+  //     return `
+  //     ## Table of Contents
+  //     - [Installation](#installation)
+  //     - [Usage](#usage)
+  //     - [Credits](#credits)
+  //     - [License](#license)
+  //     `;
+    
+  //   case 'Detailed':
+  //     return `
+  //     ## Table of Contents
+  //     - [Installation](#installation)
+  //     - [Usage](#usage)
+  //     - [Credits](#credits)
+  //     - [License](#license)
+  //     - [Badges](#badges)
+  //     - [Features](#features)
+  //     - [Contributions](#contributions)
+  //     - [Tests](#tests)
+  //     - [Contact](#contact)
+  //     `;
+  
+  //   default:
+  //     return false; // may have to return an empty string
+  // }
 }
 
 // TODO: Create a function that returns a license badge based on which license is passed in
@@ -44,14 +72,14 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  
   return `
   # ${data.title}
 
   ## Description
   ${data.description}
 
-  ## Table of Contents
-  ${data.readmeType}
+  ${renderTOC()}
   
 
   ## Installation
@@ -63,7 +91,7 @@ function generateMarkdown(data) {
   ## Credits
   ${data.credits}
   
-  ## License
+  ## License 
   ${data.license}
 
   ## Badges
